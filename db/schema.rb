a3c1f2682d59db675ab46af81712bc61647f2a08
faruuko <abfaruq02@gmail.com>
2020-08-12 17:18:08 +0100

    Removed test widget

--- db/schema.rb
+++ db/schema.rb
@@ -20,10 +20,4 @@ ActiveRecord::Schema.define(version: 2020_08_12_160041) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "widgets", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
